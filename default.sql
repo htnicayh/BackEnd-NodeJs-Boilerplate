1b1bfc4967cb73f3f60ef7e674ec0bac45f5b26f
htnicayh <hieuhpcf@gmail.com>
2021-11-06 15:07:58 +0700

    auto_increment account

--- default.sql
+++ default.sql
@@ -4,4 +4,4 @@ CREATE TABLE `account` (
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
